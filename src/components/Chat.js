457c2e0ba53ee86c9774cef3df856bddb2c530e9
Sven Hansen <76601644+1SvenHansen@users.noreply.github.com>
2022-04-28 22:55:27 +0200

    Created input fields and buttons to send scriptName triggers to drone

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,63 +1,99 @@
-import { HubConnectionBuilder } from "@microsoft/signalr";
-import { useEffect, useRef, useState } from "react";
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
 
 const Chat = () => {
-  const [connection, setConnection] = useState(null);
-  const [chat, setChat] = useState([]);
-  const latestChat = useRef(null);
+    const [missionId, setMissionId] = useState();
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+    const [altitude, setAltitude] = useState();
+    const [heading, setHeading] = useState();
+    const [order, setOrder] = useState();
 
-  latestChat.current = chat;
+    const [scriptName, setScriptName] = useState();
 
-  useEffect(() => {
-    const newConnection = new HubConnectionBuilder()
-      .withUrl("https://localhost:7000/FlightHub")
-      .withAutomaticReconnect()
-      .build();
+    const [connection, setConnection] = useState(null);
+    const [chat, setChat] = useState([]);
+    const latestChat = useRef(null);
 
-    // Check for internet
-    // If internet, connect to https://localhost:5000/FlightHub
-    // If no internet, connect to https://localhost:7000/FlightHub
+    latestChat.current = chat;
 
-    setConnection(newConnection);
-  }, []);
+    useEffect(() => {
+        // Check for internet. If internet, connect to https://localhost:5000/FlightHub. If no internet, connect to https://localhost:7000/FlightHub
+        const newConnection = new HubConnectionBuilder()
+            .withUrl('https://localhost:7001/FlightHub')
+            .withAutomaticReconnect()
+            .build();
 
-  useEffect(() => {
-    async function getData() {
-      if (connection) {
-        console.log(connection);
-        await connection
-          .start()
-          .then((result) => {
-            console.log("Connected!");
+        setConnection(newConnection);
+    }, []);
 
-            connection.on("FlightData", (message) => {
-              console.log(message);
-            });
-          })
-          .catch((e) => console.log("Connection failed: ", e));
-      }
-    }
-    getData();
-  }, [connection]);
+    useEffect(() => {
+        async function getData() {
+            if (connection) {
+                console.log(connection);
+                await connection
+                    .start()
+                    .then((result) => {
+                        console.log('Connected!');
 
-  const sendMessage = async (user, message) => {
-    const chatMessage = {
-      user: user,
-      message: message,
+                        connection.on('FlightData', (message) => {
+                            console.log(message);
+                        });
+                    })
+                    .catch((e) => console.log('Connection failed: ', e));
+            }
+        }
+        getData();
+    }, [connection]);
+
+    const sendWaypoint = () => {
+        axios.post(`https://localhost:7001/DroneCommand/Waypoint`, {
+            latitude,
+            longitude,
+            altitude,
+            heading,
+            order,
+            missionId,
+        });
     };
 
-    if (connection.connectionStarted) {
-      try {
-        await connection.send("ReceiveFlightData", chatMessage);
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      alert("No connection to server yet.");
-    }
-  };
+    const runScript = () => {
+        axios.post(`https://localhost:7001/DroneCommand/RunScript`, { scriptName });
+    };
 
-  return <div>This is our chat component</div>;
+    return (
+        <div style={{ padding: '25px' }}>
+            <label htmlFor="missionId">Mission ID</label>
+            <input type="text" onChange={(e) => setMissionId(e.target.value)} placeholder="Enter mission id" />
+            <br />
+            <label htmlFor="latitude">Latitude</label>
+            <input type="text" onChange={(e) => setLatitude(e.target.value)} placeholder="Latitude" />
+            <br />
+            <label htmlFor="longitude">Longitude</label>
+            <input type="text" onChange={(e) => setLongitude(e.target.value)} placeholder="Longitude" />
+            <br />
+            <label htmlFor="altitude">Altitude</label>
+            <input type="text" onChange={(e) => setAltitude(e.target.value)} placeholder="Altitude" />
+            <br />
+            <label htmlFor="heading">Heading</label>
+            <input type="text" onChange={(e) => setHeading(e.target.value)} placeholder="Heading" />
+            <br />
+            <label htmlFor="order">Order</label>
+            <input type="text" onChange={(e) => setOrder(e.target.value)} placeholder="Order" />
+            <br />
+            <button onClick={sendWaypoint}>Send Waypoint</button>
+
+            <br />
+            <br />
+            <br />
+
+            <label htmlFor="scriptName">Script Name</label>
+            <input type="text" onChange={(e) => setScriptName(e.target.value)} placeholder="Enter script name" />
+            <br />
+            <button onClick={runScript}>Run script</button>
+        </div>
+    );
 };
 
 export default Chat;

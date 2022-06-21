7526798fcd7781f5203dd913d265f19c2c850652
Khoa Doan <doannguyenthanhkhoa2112@gmail.com>
2022-06-21 17:00:26 +0200

    Added new changes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,102 +1,111 @@
-import { HubConnectionBuilder } from '@microsoft/signalr';
-import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import { HubConnectionBuilder } from "@microsoft/signalr";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 const Chat = () => {
-    const [missionId, setMissionId] = useState();
-    const [latitude, setLatitude] = useState();
-    const [longitude, setLongitude] = useState();
-    const [altitude, setAltitude] = useState();
-    const [heading, setHeading] = useState();
-    const [order, setOrder] = useState();
+  const [missionId, setMissionId] = useState();
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [altitude, setAltitude] = useState();
+  const [heading, setHeading] = useState();
+  const [order, setOrder] = useState();
 
-    const [scriptName, setScriptName] = useState();
-    const [coords, setCoords] = useState();
+  const [scriptName, setScriptName] = useState();
+  const [coords, setCoords] = useState();
 
-    const [connection, setConnection] = useState(null);
-    const [chat, setChat] = useState([]);
-    const latestChat = useRef(null);
+  const [connection, setConnection] = useState(null);
+  const [chat, setChat] = useState([]);
+  const latestChat = useRef(null);
 
-    latestChat.current = chat;
+  latestChat.current = chat;
 
-    useEffect(() => {
-        // Check for internet. If internet, connect to https://localhost:5000/FlightHub. If no internet, connect to https://localhost:7000/FlightHub
-        const newConnection = new HubConnectionBuilder()
-            .withUrl('https://localhost:7001/FlightHub')
-            .withAutomaticReconnect()
-            .build();
+  useEffect(() => {
+    // Check for internet. If internet, connect to https://localhost:5000/FlightHub. If no internet, connect to https://localhost:7000/FlightHub
+    const newConnection = new HubConnectionBuilder()
+      .withUrl("https://localhost:7001/FlightHub")
+      .withAutomaticReconnect()
+      .build();
 
-        setConnection(newConnection);
-    }, []);
+    setConnection(newConnection);
+  }, []);
 
-    useEffect(() => {
-        async function getData() {
-            if (connection) {
-                console.log(connection);
-                await connection
-                    .start()
-                    .then((result) => {
-                        var textarea = document.getElementById('debugMessagesTextArea');
-                        
-                        textarea.append('Connected!' + '\n');
+  useEffect(() => {
+    async function getData() {
+      if (connection) {
+        console.log(connection);
+        await connection
+          .start()
+          .then((result) => {
+            var textarea = document.getElementById("debugMessagesTextArea");
 
-                        connection.on('FlightData', (message) => {
-                            console.log(message);
-                        });
+            textarea.append("Connected!" + "\n");
 
-                        connection.on('FlightDataDebugMessage', (message) => {
-                            textarea.append(message + '\n');
-                            textarea.scrollTop = textarea.scrollHeight;
-                        });
-                    })
-                    .catch((e) => console.log('Connection failed: ', e));
-            }
-        }
-        getData();
-    }, [connection]);
+            connection.on("FlightData", (message) => {
+              console.log(message);
+            });
 
-    const eraseTextarea = () => {
-        document.getElementById('debugMessagesTextArea').value = "";
+            connection.on("FlightDataDebugMessage", (message) => {
+              console.log(message);
+              textarea.append(message + "\n");
+              textarea.scrollTop = textarea.scrollHeight;
+            });
+          })
+          .catch((e) => console.log("Connection failed: ", e));
+      }
     }
+    getData();
+  }, [connection]);
 
-    const sendWaypoint = () => {
-        axios.post(`https://localhost:7001/DroneCommand/Waypoint`, {
-            latitude,
-            longitude,
-            altitude,
-            heading,
-            order,
-            missionId,
-        });
-    };
+  const eraseTextarea = () => {
+    document.getElementById("debugMessagesTextArea").value = "";
+  };
 
-    const runScript = () => {
-        axios.post(`https://localhost:7001/DroneCommand/RunScript`, { scriptName });
-    };
+  const sendWaypoint = () => {
+    axios.post(`https://localhost:7001/DroneCommand/Waypoint`, {
+      latitude,
+      longitude,
+      altitude,
+      heading,
+      order,
+      missionId,
+    });
+  };
 
-    const startTraining = () => {
-        axios.post(`https://localhost:7001/DroneCommand/RunScript`, { scriptName: 'f_data.py' });
-    };
+  const runScript = () => {
+    axios.post(`https://localhost:7001/DroneCommand/RunScript`, { scriptName });
+  };
 
-    const startListening = () => {
-        axios.post(`https://localhost:7001/DroneCommand/StartListeningToDrone`);
-    };
+  const startTraining = () => {
+    axios.post(`https://localhost:7001/DroneCommand/RunScript`, {
+      scriptName: "f_data.py",
+    });
+  };
 
-    const sendCoords = () => {
-        axios.post(`https://localhost:7001/DroneCommand/SendTrainingCoords`, { scriptName: coords });
-    };
+  const startListening = () => {
+    axios.post(`https://localhost:7001/DroneCommand/StartListeningToDrone`);
+  };
 
-    const startMission = () => {
-        axios.post(`https://localhost:7001/DroneCommand/StartMission`, { Id: missionId });
-    };
+  const sendCoords = () => {
+    axios.post(`https://localhost:7001/DroneCommand/SendTrainingCoords`, {
+      scriptName: coords,
+    });
+  };
 
-    const endMission = () => {
-        axios.post(`https://localhost:7001/DroneCommand/EndMission`, { Id: missionId });
-    };
+  const startMission = () => {
+    axios.post(`https://localhost:7001/DroneCommand/StartMission`, {
+      Id: missionId,
+    });
+  };
 
-    return (
-        <div style={{ padding: '25px' }}>
-            {/* <label htmlFor="missionId">Mission ID</label>
+  const endMission = () => {
+    axios.post(`https://localhost:7001/DroneCommand/EndMission`, {
+      Id: missionId,
+    });
+  };
+
+  return (
+    <div style={{ padding: "25px" }}>
+      {/* <label htmlFor="missionId">Mission ID</label>
             <input type="text" onChange={(e) => setMissionId(e.target.value)} placeholder="Enter mission id" />
             <button onClick={startMission}>Start Mission</button>
             <button onClick={endMission}>End Mission</button>
@@ -118,33 +127,46 @@ const Chat = () => {
             <br />
             <button onClick={sendWaypoint}>Send Waypoint</button> */}
 
-            <br />
-            <br />
-            <label htmlFor="coords">Coords</label>
-            <input type="text" onChange={(e) => setCoords(e.target.value)} placeholder="Coords" />
-            <button onClick={sendCoords}>Send Coords</button>
-            <br />
-            <button onClick={startTraining}>Start Training</button>
-            <br />
-            <br />
-            <br />
+      <br />
+      <br />
+      <label htmlFor="coords">Coords</label>
+      <input
+        type="text"
+        onChange={(e) => setCoords(e.target.value)}
+        placeholder="Coords"
+      />
+      <button onClick={sendCoords}>Send Coords</button>
+      <br />
+      <button onClick={startTraining}>Start Training</button>
+      <br />
+      <br />
+      <br />
 
-            <label htmlFor="scriptName">Script Name</label>
-            <input type="text" onChange={(e) => setScriptName(e.target.value)} placeholder="Enter script name" />
-            <br />
-            <button onClick={runScript}>Run script</button>
+      <label htmlFor="scriptName">Script Name</label>
+      <input
+        type="text"
+        onChange={(e) => setScriptName(e.target.value)}
+        placeholder="Enter script name"
+      />
+      <br />
+      <button onClick={runScript}>Run script</button>
 
-            <br />
-            <br />
-            <br />
-            
-            <p>Debug messages<button onClick={eraseTextarea}>Clear</button></p>
-            
-            <textarea id="debugMessagesTextArea" name="debugMessages" rows="20" cols="100">
-                
-            </textarea>
-        </div>
-    );
+      <br />
+      <br />
+      <br />
+
+      <p>
+        Debug messages<button onClick={eraseTextarea}>Clear</button>
+      </p>
+
+      <textarea
+        id="debugMessagesTextArea"
+        name="debugMessages"
+        rows="20"
+        cols="100"
+      ></textarea>
+    </div>
+  );
 };
 
 export default Chat;
